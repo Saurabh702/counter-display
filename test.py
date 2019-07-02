923cad3e758455c7737f4f30e645ab682a8d9915
Saurabh702 <bhandarisaurabh800@gmail.com>
2019-07-03 00:18:58 +0530

    Auto Text Scaling

--- test.py
+++ test.py
@@ -15,12 +15,24 @@ red = (255,0,0)
 gameDisplay = pygame.display.set_mode((display_width,display_height),pygame.FULLSCREEN)
 gameDisplay.fill(white)
 
+# Reference width and height for font size 450
+ref_w, ref_h = 1366, 768 
+# Current display width and height
+cur_w, cur_h = display_width, display_height
+
+'''
 def text_objects(text, font):
     textSurface = font.render(text, True, black)
     return textSurface, textSurface.get_rect()
+''' 
+
+def text_objects(text, font):
+    textSurface = font.render(text, True, black).convert_alpha()
+    txt_w, txt_h = textSurface.get_size()
+    textSurface = pygame.transform.smoothscale(textSurface, (txt_w * cur_w // ref_w, txt_h * cur_h // ref_h))
+    return textSurface, textSurface.get_rect()  
 	
 def message_display(text):
-    #largeText = pygame.font.Font('freesansbold.ttf',450)
     largeText = pygame.font.Font('digifaw.ttf',450)
     TextSurf, TextRect = text_objects(text, largeText)
     TextRect.center = ((display_width/2),(display_height/2))
